47019fd557b871f2eb2bd10f811fda3399de3b38
mattlawlis <89171568+mattlawlis@users.noreply.github.com>
2024-08-18 04:57:39 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
 import '@openassistantgpt/ui/dist/index.css';
 
-import { Ubuntu } from 'next/font/google';
-
-const inter = Ubuntu({ subsets: ['latin'] });
+import localFont from 'next/font/local'
+ 
+// Font files can be colocated inside of `app`
+const myFont = localFont({
+  src: './TX-Law-Advisor-Regular.ttf',
+  display: 'swap',
+})
 
 export const metadata = {
-  title: 'Texas Law Advisor',
-  description: 'Texas Law Advisor',
+  title: 'George D. Reyes',
+  description: 'Chatbot',
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={myFont.className}>
+      <body>{children}</body>
     </html>
   );
 }
